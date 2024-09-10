2c2800b0e03bd7493a60193871d8dcae50d5ff98
root <root@DESKTOP-OH2Q74V>
2024-09-10 15:33:06 +0530

    added API path

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const bodyParser=require("body-parser")
+const PORT = 5000
+ 
+app.get("/",(req, res) => {
+  res.send("hello from Home.")
+})
+  
+//Adding Body Parser For JSON
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//This is dummy API
+
+app.get("/api/v1", (req, res) => {
+  res.json(
+    {
+      message: "Hello from the API Version 1"
+    }
+  )
 })
 
-app.listen(port, () => {
-  console.log(`my application is ${port}`)
-})
+
+app.listen(PORT, () => console.log('Server running on port: http://localhost:${PORT}'));
+
