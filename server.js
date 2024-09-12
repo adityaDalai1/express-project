8e07a6ccca7ba09ec3af892a0266ca6a0d9fc584
root <root@DESKTOP-OH2Q74V>
2024-09-12 10:17:52 +0530

    first again

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-const express = require('express')
-const app = express()
-const bodyParser=require("body-parser")
-const PORT = 5000
+import express from 'express';
+const app = express();
+
+import bodyParser from 'body-parser'
+
+import userRoutes from './routes/user.js'
+
+const PORT = 5000;
  
 app.get("/",(req, res) => {
   res.send("hello from Home.")
 })
   
 //Adding Body Parser For JSON
-app.use(bodyParser.json())
+
 
 //This is dummy API
 
 app.get("/api/v1", (req, res) => {
+  console.log("in the api route")
   res.json(
     {
       message: "Hello from the API Version 1"
     }
   )
 })
+app.use(bodyParser.json());
 
+app.use('/user',userRoutes);
 
-app.listen(PORT, () => console.log('Server running on port: http://localhost:${PORT}'));
+app.listen(PORT,() => {
+  console.log('server started on port ${PORT}');
+})
 
